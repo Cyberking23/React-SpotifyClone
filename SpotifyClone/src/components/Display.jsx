acef1f10363757299a7f11fdd2555fa60ca57203
Cyberking23 <102163019+Cyberking23@users.noreply.github.com>
2024-10-12 17:59:43 -0600

    Navigation buttons

--- SpotifyClone/src/components/Display.jsx
+++ SpotifyClone/src/components/Display.jsx
@@ -1,13 +1,27 @@
-import { useRef } from "react"
+import { useEffect, useRef } from "react"
 import DisplayAlbum from "./DisplayAlbum"
 import DisplayHome from "./DisplayHome"
 import {Route,Routes, useLocation} from 'react-router-dom'
+import {albumsData} from '../assets/assets'
 
 
 function Display() {
   const displayRef = useRef();
   const location = useLocation();
-  console.log(location)
+  const isAlbum = location.pathname.includes("album")
+  const albumId = isAlbum ? location.pathname.slice(-1): ""
+  const bgColor = albumsData[Number(albumId)].bgColor;
+  
+  useEffect(()=>{
+    if(isAlbum){
+      displayRef.current.style.background = `linear-gradient(${bgColor},#121212)`
+    } else{
+      displayRef.current.style.background = `#121212`
+
+    }
+  })
+  
+
   return (
     <div ref={displayRef}  className="w-[100%] m-2 px-6 pt-4 rounded bg-[#121212] text-white overflow-auto lg:w-[75%] lg:ml-0">
       <Routes>
